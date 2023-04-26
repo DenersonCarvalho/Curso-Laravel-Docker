a373107e880856fa5202e7b0034ef0d2aabee916
Dener <dener.carvalhoo@gmail.com>
2023-04-26 20:30:29 -0300

    curso-laravel

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,9 @@
-dgsrndndtyndty
+<h1>Lista de Usuários</h1>
+<ul>
+    @foreach($users as $user)
+    <li>
+        {{$user->name}} - {{$user->email}} - {{$user->created_at}}
+        <a href="{{route('users.show', $user->id)}}">Detalhes</a>
+    </li>
+    @endforeach
+</ul>
